d60ab851ef9ea28b5d4b231673be66e9be1804e1
tanishka-td <tanishkadass02@gmail.com>
2026-08-01 00:47:52 +0530

    ques 3014 30/07/26

--- 3014_Minimum_Number_of_Pushes_to_Type_Word_I/solution.java
+++ 3014_Minimum_Number_of_Pushes_to_Type_Word_I/solution.java
@@ -1,13 +1,9 @@
 class Solution {
-    public int numberOfPoints(List<List<Integer>> nums) {
-        Set<Integer> points = new HashSet<>();
-        for (List<Integer> car : nums) {
-            int start = car.get(0);
-            int end = car.get(1);
-            for (int i = start; i <= end; i++) {
-                points.add(i);
-            }
+    public int minimumPushes(String word) {
+        int ans = 0;
+        for (int i=0; i<word.length(); i++) {
+            ans += i/8+1;
         }
-        return points.size();
+        return ans;
     }
 }
